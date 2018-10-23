bce0d5899946f417c213d99065264f4a67bd8083
Fornito Marvin <marvin.fornito@gmail.com>
2018-10-23 08:09:31 +0200

    Création de la classe Personne.

---
+++ TP4-Carnet/src/MainCarnetAdresse/Personne.java
@@ -0,0 +1,38 @@
+package MainCarnetAdresse;
+
+public class Personne {
+	
+	private String nom, prenom, adresse;
+	
+	public Personne(String nom, String prenom, String adresse)
+	{
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+}
