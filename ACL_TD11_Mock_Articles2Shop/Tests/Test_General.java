44e0a8519e9f1917eb6210a5d9fc3827e494446f
Lukyky414 <lucassschwab.e@gmail.com>
2018-11-08 15:15:49 +0000

    Premier Test fonctionnel
    La suite pourrait peut etre arriver un jours

--- ACL_TD11_Mock_Articles2Shop/Tests/Test_General.java
+++ ACL_TD11_Mock_Articles2Shop/Tests/Test_General.java
@@ -1,9 +1,37 @@
+import Classes.Articles2Shop;
+import Classes.Currency;
+import Classes.StockService;
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class Test_General {
+	StockService stock_mock;
+	Articles2Shop shop;
+
+	@Before
+	public void creation(){
+		stock_mock = EasyMock.createStrictMock(StockService.class);
+		shop = new Articles2Shop(stock_mock);
+	}
 
 	@Test
-	public void function(){
-		
+	public void addArticleStrictQuantityNormal(){
+		String article = "chaussure";
+		Currency prix = new Currency(10, "€");
+		EasyMock.expect(stock_mock.available(article)).andReturn(5);
+
+		stock_mock.buy(article);
+		stock_mock.buy(article);
+		stock_mock.buy(article);
+
+		EasyMock.expect(stock_mock.getPrix(article)).andReturn(prix);
+
+		EasyMock.replay(stock_mock);
+
+		assertEquals(3, shop.addArticleStrictQuantity(article, 3));
+
+		EasyMock.verify(stock_mock);
 	}
 }
